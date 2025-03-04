43d17d61e9c042545660ed4f1a026fb50a749881
RAVIBHARATHI P C <131471282+ravibharathi656@users.noreply.github.com>
2025-03-04 19:01:59 +0530

    feat: add footer navigation

--- pwa/src/pages/LandingPage.vue
+++ pwa/src/pages/LandingPage.vue
@@ -3,8 +3,8 @@
 	  <div class="w-full flex flex-col">
 		<div class="w-full sm:w-96 bg-white h-14 shadow-lg fixed top-0 z-10">
 		  <div class="p-2 flex pt-3">
-			<FeatherIcon class="w-6 h-6 mt-1 text-gray-600 hover:text-black" name="list" />
-			<div class="relative w-full">
+			<!-- <FeatherIcon class="w-6 h-6 mt-1 text-gray-600 hover:text-black" name="list" /> -->
+			<div class="relative w-[80rem]">
 			  <FormControl
 				:type="'text'"
 				size="sm"
@@ -13,13 +13,13 @@
 				:disabled="false"
 				v-model="inputValue"
 				@focus="showDropdown = true"
-				class="flex-grow pl-4 w-full pt-[3px]"
+				class="flex-grow pl-1 w-full pt-[3px]"
 			  >
 				<template #prefix>
 				  <FeatherIcon class="w-4" name="search" />
 				</template>
 			  </FormControl>
-			  <div v-if="showDropdown" class="absolute left-[1rem] right-0 bg-white border border-gray-200 rounded-lg shadow-lg mt-1 z-20">
+			  <div v-if="showDropdown" class="absolute w-full right-0 bg-white border border-gray-200 rounded-lg shadow-lg mt-1 z-20">
 				<div v-if="options.length > 0" v-for="option in options" :key="option" class="p-2 hover:bg-gray-200 cursor-pointer" @click="selectOption(option)">
 					<p class=" text-[14px] font-medium truncate">
 						{{ option.doctype }}
@@ -44,27 +44,63 @@
 		</div>
 		<div class="mt-14 w-full h-full">
 		  <div class="p-4">
-			<p class="font-medium text-xl pl-1 text-gray-600">Quick Link</p>
-			<div class="bg-white h-[28rem] w-full rounded-lg mt-2 overflow-y-auto no-scrollbar p-2">
-				<div v-for="link in links" :key="link.doctype" class="border-gray-200 border-b-[1.5px]">
-					<div class="p-2 flex pt-2 touchable" @click="navigateToForm(link)">
-						<FeatherIcon name="link" class="text-gray-600 h-5 w-5" />
-						<p class="text-gray-600 pl-3 truncate w-[8rem]">{{ link.doctype }}</p>
-						<div class="ml-auto touchable" @click.stop="handelNew(link)">
-							<FeatherIcon name="plus" class="text-gray-600 h-5 w-5 hover:text-black" />
+			
+			<div v-if="selectedTab === 'Quick Link'">
+				<div class="bg-white h-[28rem] w-full rounded-lg mt-2 overflow-y-auto no-scrollbar p-2">
+					<div v-for="link in links" :key="link.doctype" class="border-gray-200 border-b-[1.5px]">
+						<div class="p-2 flex pt-2 touchable" @click="navigateToForm(link)">
+							<FeatherIcon name="link" class="text-gray-600 h-5 w-5" />
+							<p class="text-gray-600 pl-3 truncate w-[8rem]">{{ link.doctype }}</p>
+							<div class="ml-auto touchable" @click.stop="handelNew(link)">
+								<FeatherIcon name="plus" class="text-gray-600 h-5 w-5 hover:text-black" />
+							</div>
 						</div>
-					</div>
-				</div>				  
+					</div>				  
+				</div>
+			</div>
+			<div v-else-if="selectedTab === 'Dashboard'">
+				<div class=" h-[28rem] w-full rounded-lg mt-2 overflow-y-auto no-scrollbar p-1">
+					<p>hi</p>
+				</div>
 			</div>
 		  </div>
 		</div>
-		<div v-if="ifError" class='fixed bottom-[4rem] leading-5 pr-[60rem] pl-[2.5rem] z-50 w-[20rem] sm:w-96 animate-slide-in'>
+		<div v-if="ifError" class='fixed bottom-[3rem] leading-5 pr-[60rem] pl-[2.5rem] z-50 w-[20rem] sm:w-96 animate-slide-in'>
 			<div class="rounded w-[20rem] h-fit p-2 text-left bg-red-200 text-red-500">
 				<div class=" flex">
 					<div class=" h-full">
 						<FeatherIcon name="x" class="inline w-6 h-6 mr-2 hover:cursor-pointer" @click="close"/>
 					</div>
 					<span v-html="Error"></span>
+				</div>
+			</div>
+		</div>
+
+		<div class='fixed bottom-0 w-full sm:w-96 bg-white h-14 shadow-lg z-10 rounded-t-md'>
+			<div class="p-2 flex w-full items-center h-full">
+				<div class="flex items-center w-full justify-between">
+					<div class=" flex flex-col h-full w-[50%] items-center justify-center"
+						 :class="selectedTab === 'Quick Link' ? 'text-gray-600  text-md font-bold' : 'text-gray-500 text-sm font-medium'"
+					>
+						<FeatherIcon class="w-4" name="link"   />
+						<p 
+							class="cursor-pointer"
+							@click="selectedTab = 'Quick Link'"
+						>
+							Quick Link
+						</p>
+					</div>
+					<div class=" flex flex-col h-full w-[50%] items-center justify-center"
+						 :class="selectedTab === 'Dashboard' ? 'text-gray-600  text-md font-bold' : 'text-gray-500 text-sm font-medium'"
+					>
+						<FeatherIcon class="w-4" name="layout"/>
+						<p 
+							class="cursor-pointer"
+							@click="selectedTab = 'Dashboard'"
+						>
+							Dashboard
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -85,7 +121,7 @@ import { apiRetrival } from '../utils/apiRetrival'
 
 const store = landingPage();
 const links = ref(store.links);
-
+const selectedTab = ref('Quick Link');
 
 onMounted( async () => {
 	const apiValue =  await apiRetrival();
