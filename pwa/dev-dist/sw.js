db2883e1dda88b775fc8f8f478c5a7f33925e13d
RAVIBHARATHI P C <131471282+ravibharathi656@users.noreply.github.com>
2025-03-10 18:16:52 +0530

    chore: build project

--- pwa/dev-dist/sw.js
+++ pwa/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-51d6c265'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gm6fuh69s28"
+    "revision": "0.9t4fgg19e48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
